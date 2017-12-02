0a825afee9b30758f009001556e14d673fa99870
klasing <klasing1959@gmail.com>
2017-12-02 15:21:15 +0100

    02-12-2017 15:20 inserting under dinner from DinnerActivity

--- app/src/main/java/com/example/koetshuiskoken/DinnerActivity.java
+++ app/src/main/java/com/example/koetshuiskoken/DinnerActivity.java
@@ -5,13 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import com.firebase.client.Firebase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class DinnerActivity extends AppCompatActivity {
     //************************************************************************
     //*                 declare
     //************************************************************************
     private static final String LOG_TAG = "***" + DinnerActivity.class.getSimpleName();
+
+    private Firebase mFirebaseRef;
 
     //************************************************************************
     //*                 onCreate
@@ -37,6 +47,10 @@ public class DinnerActivity extends AppCompatActivity {
         String strDateDay = bundle.getString("date_day");
         TextView date_day = findViewById(R.id.date_day);
         date_day.setText(strDateDay);
+
+        final String url = bundle.getString("FIREBASE_URL");
+
+        mFirebaseRef = new Firebase(url);
 
         //********************************************************************
         // testing the JsonObject
@@ -72,6 +86,47 @@ public class DinnerActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.i(LOG_TAG, "button_save.onClick()");
+                //************************************************************
+                // test sample
+//                List<String> listEating = new ArrayList<>();
+//                listEating.add("Anne");
+//                listEating.add("Mark");
+//                JsonObject jsonObject = new JsonObject("SUN", "12-03-2017",
+//                        "Jim", "Meatloaf", (ArrayList<String>)listEating);
+                //************************************************************
+                JsonObject jsonObject = new JsonObject();
+                // get name_day into json
+                TextView name_day = (TextView) findViewById(R.id.name_day);
+                jsonObject.setNameDay(name_day.getText().toString());
+                // get date day into json
+                TextView date_day = (TextView) findViewById(R.id.date_day);
+                jsonObject.setDateDay(date_day.getText().toString());
+                // get name cook into json
+                EditText edittext_cook = (EditText) findViewById(R.id.edittext_cook);
+                jsonObject.setNameCook(edittext_cook.getText().toString());
+                // get description meal into json
+                EditText edittext_meal = (EditText) findViewById(R.id.edittext_meal);
+                jsonObject.setDescriptionMeal(edittext_meal.getText().toString());
+                // get guests into json
+                EditText edittext_eating = (EditText) findViewById(R.id.edittext_eating);
+                String strEating = edittext_eating.getText().toString();
+                if (strEating.length() > 0) {
+                    if (strEating.charAt(strEating.length() - 1) != '\n') strEating += "\n";
+                    List<String> listEating = new ArrayList<String>();
+                    String regex = "\n";
+                    Pattern p = Pattern.compile(regex);
+                    Matcher m = p.matcher(strEating);
+                    int i = 0;
+                    int iStartSubstr = 0;
+                    while (m.find()) {
+                        String strSubstr = strEating.substring(iStartSubstr, m.start());
+                        listEating.add(strSubstr);
+                        iStartSubstr = m.end();
+                    }
+                    jsonObject.setEating((ArrayList<String>) listEating);
+                }
+                // place new dinner under /KoetshuisKoken2017/dinner
+                mFirebaseRef.child("dinner").push().child(date_day.getText().toString()).setValue(jsonObject);
                 // finish this activity and return to MainActivity
                 finish();
             }
--- app/src/main/java/com/example/koetshuiskoken/MainActivity.java
+++ app/src/main/java/com/example/koetshuiskoken/MainActivity.java
@@ -3,24 +3,54 @@ package com.example.koetshuiskoken;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.firebase.client.Firebase;
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
     //************************************************************************
     //*                 declare
     //************************************************************************
     private static final String LOG_TAG = "***" + MainActivity.class.getSimpleName();
+    public static final String FIREBASE_URL = "https://koetshuiskoken2017.firebaseio.com/";
+    public static final String ANONYMOUS = "anonymous";
+    public static final int RC_SIGN_IN = 1;
 
     // adapter for the list day of week
     private DayOfWeekAdapter mAdapter;
 
     protected static Context context;
+
+    private ListView dowListView;
+
+    private String mUsername;
+
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mMessagesDatabaseReference;
+    private ChildEventListener mChildEventListener;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+
+    private Firebase mRef;
 
     //************************************************************************
     //*                 onCreate
@@ -32,8 +62,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         context = this.getApplicationContext();
 
+        // necessary for transferring Firebase to DinnerActivity
+        Firebase.setAndroidContext(this);
+        // create firebase reference
+        mRef = new Firebase(FIREBASE_URL);
+
+        mUsername = ANONYMOUS;
+
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mMessagesDatabaseReference =
+            mFirebaseDatabase.getReference().child("dinner");
+
+        // start log in procedure
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // user is signed in
+                    onSignedInInitialize(user.getDisplayName());
+                } else {
+                    // user is signed out
+                    onSignedOutCleanup();
+                    startActivityForResult(AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setIsSmartLockEnabled(false)
+                        .setAvailableProviders(
+                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                        .build(),
+                        RC_SIGN_IN);
+                }
+            }
+        };
+
         // Find a reference to the {@link ListView} in the layout.
-        ListView dowListView = (ListView) findViewById(R.id.list);
+        dowListView = (ListView) findViewById(R.id.list);
         // Create a new adapter that takes an empty listDayOfWeek as input.
         mAdapter = new DayOfWeekAdapter(this, new ArrayList<MyDayOfWeek>());
         // Set the adapter on the {@link ListView}
@@ -47,15 +112,19 @@ public class MainActivity extends AppCompatActivity {
                 Log.i(LOG_TAG, "DayOfWeek[" + position + "].onItemClick()");
                 // Find the current DayOfWeek that was clicked on
                 MyDayOfWeek currentDayOfWeek = mAdapter.getItem(position);
+
+                // start new activity with necessary data
                 Intent intentDinnerActivity = new Intent(MainActivity.this,
                         DinnerActivity.class);
                 setExtras(intentDinnerActivity,
                         currentDayOfWeek.getColor(),
                         currentDayOfWeek.getNameOfDay(),
-                        currentDayOfWeek.getDate());
+                        currentDayOfWeek.getDate(),
+                        FIREBASE_URL);
                 startActivity(intentDinnerActivity);
             }
         });
+
         // Add a array of {@link DayOfWeek}s to the adapter's
         // data set. This will trigger the ListView to update.
         mAdapter.addAll(QueryUtils.fetchDayOfWeekData());
@@ -67,11 +136,128 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Bring the GUI characteristics to the activity.
      */
-    private void setExtras(Intent intent, int iColor,
-                           String strNameDay, String strDateDay) {
+    private void setExtras(Intent intent, int iColor, String strNameDay,
+        String strDateDay, String firebaseUrl) {
         Log.i(LOG_TAG, "setExtras()");
         intent.putExtra("color", iColor);
         intent.putExtra("name_day", strNameDay);
         intent.putExtra("date_day", strDateDay);
+        intent.putExtra("FIREBASE_URL", firebaseUrl);
+    }
+
+    //************************************************************************
+    //*                 onResume
+    //************************************************************************
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(LOG_TAG, "onResume()");
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    //************************************************************************
+    //*                 onPause
+    //************************************************************************
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(LOG_TAG, "onPause()");
+        if (mAuthStateListener != null) {
+            mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+        }
+        detachDatabaseReadListener();
+    }
+
+    //************************************************************************
+    //*                 onCreateOptionsMenu
+    //************************************************************************
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        Log.i(LOG_TAG, "onCreateOptionsMenu()");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    //************************************************************************
+    //*                 onOptionsItemSelected
+    //************************************************************************
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i(LOG_TAG, "onOptionsItemSelected()");
+        switch (item.getItemId()) {
+            case R.id.sign_out_menu:
+                Log.i(LOG_TAG, "Sign out from Firebase");
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //************************************************************************
+    //*                 onSignedInInitialize
+    //************************************************************************
+    private void onSignedInInitialize(String username) {
+        mUsername = username;
+        Log.i(LOG_TAG, "onSignedInInitialize() " + mUsername);
+        attachDatabaseReadListener();
+        //********************************************************************
+        // test database
+//        List<String> listEating = new ArrayList<>();
+//        listEating.add("Anne");
+//        listEating.add("Mark");
+//        JsonObject jsonObject = new JsonObject("SAT", "12-02-2017",
+//                "Jim", "Soup", (ArrayList<String>)listEating);
+//        mMessagesDatabaseReference.push().setValue(jsonObject);
+        //********************************************************************
+    }
+
+    //************************************************************************
+    //*                 onSignedOutCleanup
+    //************************************************************************
+    private void onSignedOutCleanup() {
+        Log.i(LOG_TAG, "onSignedOutCleanup()");
+        mUsername = ANONYMOUS;
+        detachDatabaseReadListener();
+    }
+
+    //************************************************************************
+    //*                 attachDatabaseReadListener
+    //************************************************************************
+    private void attachDatabaseReadListener() {
+        Log.i(LOG_TAG, "attachDatabaseReadListener()");
+        if (mChildEventListener == null) {
+            mChildEventListener = new ChildEventListener() {
+                @Override
+                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                    Log.i(LOG_TAG, "onChildAdded()");
+                }
+                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    Log.i(LOG_TAG, "onChildChanged()");
+                }
+                public void onChildRemoved(DataSnapshot dataSnapshot) {
+                    Log.i(LOG_TAG, "onChildRemoved()");
+                }
+                public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                    Log.i(LOG_TAG, "onChildMoved()");
+                }
+                public void onCancelled(DatabaseError databaseError) {
+                    Log.i(LOG_TAG, "onCancelled()");
+                }
+            };
+            mMessagesDatabaseReference.addChildEventListener(mChildEventListener);
+        }
+    }
+
+    //************************************************************************
+    //*                 detachDatabaseReadListener
+    //************************************************************************
+    private void detachDatabaseReadListener() {
+        Log.i(LOG_TAG, "detachDatabaseReadListener()");
+        if (mChildEventListener != null) {
+            mMessagesDatabaseReference.removeEventListener(mChildEventListener);
+            mChildEventListener = null;
+        }
     }
 }
